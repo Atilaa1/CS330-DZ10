c3247ccd401f9ce3cf393728cdc0607d588d6a9c
Atilaa1 <atila.kujundzic.5000@metropolitan.ac.rs>
2024-05-16 00:43:56 +0200

    Initial commit

---
+++ app/src/main/java/rs/ac/metropolitan/cs330_dz10/MainActivity.kt
@@ -0,0 +1,38 @@
+package rs.ac.metropolitan.cs330_dz10
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.compose.AppTheme
+
+
+class MainActivity : ComponentActivity() {
+    lateinit var navController: NavHostController
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppTheme {
+                navController = rememberNavController()
+                Scaffold(topBar = {
+                    TopAppBar(title = { Text(text = "Posiljke list") })
+                }) { innerPadding ->
+                    NavSettup(navController, innerPadding)
+                }
+            }
+        }
+    }
+}
